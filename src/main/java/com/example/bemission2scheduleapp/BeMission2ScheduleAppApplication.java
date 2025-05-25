e1402140e435128ef4a749a64f6d8366f1f21f22
sungchan <awdr1057@naver.com>
2025-05-25 14:29:56 +0900

    init commit

---
+++ src/main/java/com/example/bemission2scheduleapp/BeMission2ScheduleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bemission2scheduleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMission2ScheduleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeMission2ScheduleAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bemission2scheduleapp/BeMission2ScheduleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bemission2scheduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMission2ScheduleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
